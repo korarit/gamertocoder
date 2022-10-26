6c6b2ff7c9e7c09ec27ae5c58621481e29fd7c84
TheStepKla <krtza1@gmail.com>
2022-10-26 22:09:33 +0700

    update 26/10/65 4

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 async function random_assets (){
     //สุ่ม background image จาก backend
     const background = await fetch('/json/background.json');
-    const json = await background.json();
+    const background_json = await background.json();
 
-    console.log(json["background"])
-    var random = Math.floor(Math.random() * ((json["background"].length - 1) - 0 + 1)) + 0;
+    console.log(background_json["background"])
+    var random = Math.floor(Math.random() * ((background_json["background"].length - 1) - 0 + 1)) + 0;
     
-    document.getElementById("body_main").setAttribute("background", json["background"][random]);
+    document.getElementById("body_main").setAttribute("background", background_json["background"][random]);
 
     //สุ่ม logo
-
+    const logo = await fetch('/json/logo.json');
+    const logo_json = await logo.json();
 }
